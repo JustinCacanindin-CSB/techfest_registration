55e4c48df47278ac7326561e55b30223e7795e45
Justin Cacanindin <justinchristian.cacanindin@benilde.edu.ph>
2025-11-06 19:26:23 +0800

    Task 1: Done

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,3 +1,21 @@
 # Task 1 done
 
-print ("Hello world")
+
+print("Welcome to SMIT TechFest!")
+print("Organized by Justin_Cacanindin of APPDAET BTCS1\n")
+
+participant_input = input("How many participants will register? ")
+
+
+if participant_input.isdigit():
+    participant_count = int(participant_input)
+else:
+    print("Invalid number of participants.")
+    exit()
+
+
+if participant_count <= 0:
+    print("Invalid number of participants.")
+    exit()
+
+
